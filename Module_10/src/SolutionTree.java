433b995dcebf170feb38e3b7f7264922ff94a8d2
MRQuEuE123 <sysovmax@gmail.com>
2025-01-20 17:31:36 +0300

    change Module 10

--- Module_10/src/SolutionTree.java
+++ Module_10/src/SolutionTree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class SolutionTree {
     static int answer = 0;
 
@@ -50,6 +52,44 @@ public class SolutionTree {
         return Math.max(maxLeftPath, maxRightPath) + node.val;
     }
 
+    public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1==null ) return root2;
+        if(root2==null) return root1;
+        TreeNode root= new TreeNode(root1.val+root2.val);
+        root.left=mergeTrees(root1.left,root2.left);
+        root.right=mergeTrees(root1.right,root2.right);
+        return root;
+    }
+
+    public static TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return null;
+        else if(root.val==val)return root;
+        else if (val <root.val)return searchBST(root.left,val);
+        else return searchBST(root.right,val);
+    }
+    public static List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                assert node != null;
+                if (i == levelSize - 1) {
+                    result.add(node.val);
+                }
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(10);
         root.left = new TreeNode(5);
@@ -62,6 +102,11 @@ public class SolutionTree {
         int targetSum = 18;
         System.out.println("Максимальный путь в дереве: " + result);
         System.out.println("есть ли путь от корня до листа равный  " + targetSum + " :" + hasPathSum(root, targetSum));
+        TreeNode roo1= new TreeNode(1);
+        TreeNode roo=mergeTrees(roo1, root);
+        System.out.println(roo.val);
+        System.out.println(Collections.unmodifiableList(rightSideView(roo)));
+
 
     }
 
